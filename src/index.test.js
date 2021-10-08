3fc2509abe0fe43cb8fcd40f7d1641c693929ac6
dstock3 <dstock3@gmail.com>
2021-10-08 18:29:15 -0500

    Created initial series of tests for the caesar function.

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,7 @@
 import { capitalize, reverseString, calculator, analyze, caesar } from './index'
 
+// Capitalize 
+
 test('capitalize the first letter of a string', () => {
     expect(capitalize("david")).toBe("David");
 });
@@ -7,6 +9,8 @@ test('capitalize the first letter of a string', () => {
 test('works with spaces', () => {
     expect(capitalize('the first letter of this sentence should be capitalized.')).toBe('The first letter of this sentence should be capitalized.')
 });
+
+// ReverseString
 
 test('takes a string and returns it reversed', () => {
     expect(reverseString("mittens")).toBe("snettim");
@@ -19,6 +23,8 @@ test('works with long strings', () => {
 test('reverse a string and make sure it works on palindromes', () => {
     expect(reverseString("level")).toBe("level");
 });
+
+// Calculator
 
 test('addition', () => {
     expect(calculator.add(1, 2)).toBe(3);
@@ -36,14 +42,27 @@ test('divide', () => {
     expect(calculator.divide(30, 5)).toBe(6);
 });
 
+// Analyze
+
 test('analysis', () => {
-    expect(analyze([0, 1, 2, 3, 4, 5])).toStrictEqual({average: 2.5, min: 0, max: 5, length: 6})
+    expect(analyze([0, 1, 2, 3, 4, 5])).toStrictEqual({average: 2.5, min: 0, max: 5, length: 6});
 });
 
 test('new analysis', () => {
-    expect(analyze([7, 22, 13, 23, 4])).toStrictEqual({average: 13.8, min: 4, max: 23, length: 5})
+    expect(analyze([7, 22, 13, 23, 4])).toStrictEqual({average: 13.8, min: 4, max: 23, length: 5});
 });
 
 test('odin analysis', () => {
-    expect(analyze([1,8,3,4,2,6])).toStrictEqual({average: 4, min: 1, max: 8, length: 6})
+    expect(analyze([1,8,3,4,2,6])).toStrictEqual({average: 4, min: 1, max: 8, length: 6});
 });
+
+// Caesar
+
+test('shift 1', () => {
+    expect(caesar("mittens", 1)).toBe("njuufot")
+});
+
+test('shift 2', () => {
+    expect(caesar("mittens", 2)).toBe("okvvgpu")
+})
+
